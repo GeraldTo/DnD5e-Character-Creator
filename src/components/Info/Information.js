67567c45fc1e2d2215e7137438a4256b3bab2379
GeraldTo <46791787+GeraldTo@users.noreply.github.com>
2021-02-19 22:54:13 -0600

    languages
    Adds language object
    uses number of possible languages to learn to create selects
    resets after changing race
    future must include background

--- src/components/Info/Information.js
+++ src/components/Info/Information.js
@@ -1,19 +1,18 @@
-import React,{useState, useEffect} from 'react'
+import React from 'react'
 import Name from './Name'
 import Alignment from './Alignment'
 import Languages from './Languages'
 import Background from './Background'
 
 export default function Information(props) {
-    
+
     return (
         <div className="Information">
             <h2>Choose Information</h2>
             <Name setName={props.setName}/>
             <Alignment race={props.race} url={props.url+'alignments/'} alignment={props.alignment} setAlignment={props.setAlignment} />
             <Background classType={props.classType}/>
-            <Languages race={props.race} setLang={props.setLang} lang={props.lang} />
-            
+            <Languages url={props.url+'languages/'} race={props.race} setLang={props.setLang} lang={props.lang} />
         </div>
     )
 }
--- src/components/Info/Languages.js
+++ src/components/Info/Languages.js
@@ -1,26 +1,64 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React,{useState, useEffect} from 'react'
+import axios from 'axios'
 
 export default function Languages(props) {
-    const [LangCount, setLandCount] = useState(0)
+    const [api, setapi] = useState(null)
+    const [raceLang, setRaceLang] = useState([])
+    const [langCounter, setLangCounter] = useState([])
+    
     useEffect(() => {
-        let languages = []
+        axios.get(props.url)
+            .then(response=>{
+                setapi(response.data)
+            })
+    }, [])
+    useEffect(() => {
+        let known = []
         for (let i = 0; i < props.race.languages.length; i++) {
-            languages.push(props.race.languages[i].index)
+            known.push(props.race.languages[i].index)
         } 
-        props.setLang(languages)
+        setRaceLang(known) 
+        props.setLang(known) 
         let langCount = 0
         if(props.race){
             langCount = props.race.language_options ? props.race.language_options.choose : 0
         }
-        setLandCount(langCount)
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        let selectedLang = []
+        selectedLang.length = langCount
+        setLangCounter(selectedLang)
     }, [props.race])
+    
+    function handleAdd(e,i) {
+        let all = [...langCounter]
+        all[i]= e.currentTarget.value ? e.currentTarget.value: null
+        setLangCounter(all) 
+        props.setLang(raceLang.concat(all.filter(function(el) { return el; })))
+    }
+    function multiLang() {
+        let langs = []
+        if(api){
+
+            for (let i = 0; i <langCounter.length; i++) {
+                let langList = []
+                langList.push(<option key={-1} value="">{langCounter[i] ? langCounter[i]:'--Select Lang--'}</option>)
+                langCounter[i] && langList.push(<option key={api.count} value="">--Select Lang--</option>)
+                for (let j = 0; j < api.count; j++) {
+                    props.lang.indexOf(api.results[j].index)<0 && langList.push(
+                        <option key={j}> {api.results[j].index}</option>)
+                }
+                langs.push(<select key={i} onChange={e => handleAdd(e,i)} >{langList}</select> )
+            }
+        }
+        return langs
+    }
+
     return (
         <div>
             <h3>Choose Langauges</h3>
-            {LangCount}<br />
             <h4>Race Languages:</h4> {props.race.language_desc}<br />
-            <h4>Current Languages:</h4> {props.lang.join(', ')}
+            <h4>Current Languages:</h4> {props.lang.join(', ')}<br/>
+            {multiLang()}<br/>
         </div>
     )
 }
